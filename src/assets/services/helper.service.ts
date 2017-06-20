0545b45c8374d01c0ec76b831bc6e732c7899c8b
Stephanus van Wyk <svw88@hotmail.com>
2017-06-20 17:16:23 -0300

    first commit

--- src/assets/services/helper.service.ts
+++ src/assets/services/helper.service.ts
@@ -6,8 +6,10 @@ export class HelperService {
     constructor() { }
 
     checkPalindrome(input) {
-        input = input.toLowerCase().replace(/[^\w\s]/gi, '');
+        input = input.toLowerCase().replace(/\s/gi,'').replace(/[^\w\s]/gi, '');
+        console.log(input);
         var reverse = input.split("").reverse().join("");
+        console.log(reverse);
         if (reverse == input) {
             return true;
         } else {
